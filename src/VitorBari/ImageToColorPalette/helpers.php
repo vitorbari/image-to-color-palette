7e6c0fdba5b9b90a4182719bf2eca339a8752c8c
Vitor Bari Buccianti <vitorbari@gmail.com>
2016-07-05 09:24:49 -0400

    Applied fixes from StyleCI

--- src/VitorBari/ImageToColorPalette/helpers.php
+++ src/VitorBari/ImageToColorPalette/helpers.php
@@ -1,20 +1,17 @@
 <?php
 
-if ( ! function_exists('colors_table')) {
-
-	function colors_table($colors = array())
-	{
-		$html = '<table>';
-		foreach ($colors as $color) 
-		{
-			$html .= '	<tr>
+if (! function_exists('colors_table')) {
+    function colors_table($colors = array())
+    {
+        $html = '<table>';
+        foreach ($colors as $color) {
+            $html .= '	<tr>
 							<td>'.$color.'</td>
 							<td style="background-color:'.$color.';">&nbsp;&nbsp;</td>
 						</tr>';
-		}
-		$html .= '</table>';
+        }
+        $html .= '</table>';
 
-		return $html;
-	}
-
+        return $html;
+    }
 }
